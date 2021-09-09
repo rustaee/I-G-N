99f584c34ec817757722d17914942c5297a46475
Roustaei <mahdieh.roustaei@gmail.com>
2021-09-09 12:44:35 +0200

    API

--- src/App.vue
+++ src/App.vue
@@ -4,16 +4,47 @@
     <header>
         <h1>Please enter your license plate number</h1>
         <div class="plate">
-          <input type="text" class="plate__input">
-          <button class="plate__sendbtn">Send</button>
+          <input type="text" class="plate__input" v-model="plateNumber">
+          <button class="plate__sendbtn" @click="getCarInfo()">Send</button>
         </div>
     </header>
+
     <!--API -->
     <div class="api">
       <div class="api__result">
-        dfdsd
+        <!-- Error --> 
+        <div v-if="error" class="error">
+          <h3>We couldn't find any information. please make sure you've entered the right plate number</h3>
+        </div>
+
+        <!-- Loading -->
+        <div v-else-if="loading" class="loading">
+          <font-awesome-icon icon="spinner" />
+        </div>
+
+        
+        <!-- Data -->
+        <div v-else class="data">
+          <div class="api__title" v-if="!admissionDate">enter plate number to get these information.</div>
+          <div class="info">
+            <div class="info__title">Trade name</div>
+            <div class="info__value">{{ tradeName }}</div>
+          </div>
+
+          <div class="info">
+            <div class="info__title">Date of first admission</div>
+            <div class="info__value">{{ admissionDate}}</div>
+          </div>
+
+          <div class="info">
+            <div class="info__title">Fuel description</div>
+            <div class="info__value">{{ fuel }}</div>
+          </div>
+        </div>
+
       </div>
     </div>
+
     <!--Slider -->
     <div class="slider">
       <div class="slider--prev"><font-awesome-icon icon="chevron-left" /></div>
@@ -26,12 +57,48 @@
       </div>
       <div class="slider--next"><font-awesome-icon icon="chevron-right" /></div>
     </div>
+
     <!-- Footer -->
     <footer>
       <p>Copyright <span>&copy;</span> 2020-2021 All rights reserved</p>
     </footer>
   </div>
 </template>
+<script>
+export default {
+  data(){
+    return{
+      apiKey: '76d237b0f1cc4a6ddaa41f79cd3345fc09c2c0a2b907ad7184d775b2615b6b3f',
+      apiUrl: 'https://api.overheid.io/voertuiggegevens',
+      plateNumber: null,
+      tradeName: null,
+      admissionDate: null,
+      fuel: null,
+      error: false,
+      loading: false,
+    }
+  },
+  methods: {
+    getCarInfo(){
+      this.loading = true,
+      this.error = false,
+
+      fetch(`${this.apiUrl}/${this.plateNumber}?ovio-api-key=${this.apiKey}`)
+      .then(response => response.json())
+      .then(data => {
+        this.loading = false;
+        this.tradeName = data.handelsbenaming;
+        this.admissionDate = data.datum_eerste_toelating,
+        this.fuel = data.brandstof[0]['brandstof_omschrijving'];
+      })
+      .catch(error => {
+        this.error = true
+        console.log(error);
+        })
+    }
+  }
+}
+</script>
 
 <style lang="scss">
 @import url('https://fonts.googleapis.com/css2?family=Roboto:wght@300&display=swap');
@@ -53,21 +120,22 @@ input, button {
   border: none;
 }
 
-#app {
-  font-family: $font-roboto;
+body{
+  font-family: $font-roboto;  
+  background: $bg;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: $font-color;
-  background: $bg;
   font-size: $font-size;
+  line-height: 30px;
 }
 
 .container{
   width: 100vw;
-  height: 100vh;
+  min-height: 100vh;
   display: grid;
-  grid-template: auto 30% 30% minmax(50px, 10%)/ 1fr;
+  grid-template: auto minmax(250px, 1.5fr) 0.7fr auto/ 1fr;
   grid-template-areas: "header"
   "api"
   "slider"
@@ -113,6 +181,7 @@ header{
   color: $font-color;
 }
 
+/** Api */
 .api{
   grid-area: api;
   display: flex;
@@ -121,9 +190,15 @@ header{
 
 }
 
+.api__title{
+  text-align: center;
+  color: $primary-color;
+  line-height: 20px;
+  font-size: $font-size - 2px;
+}
+
 .api__result{
   width: 80%;
-  height: 100%;
   background: #fff;
   box-shadow: 0px 5px 5px rgba(0, 0, 0, 0.1);
   padding: 20px;
@@ -133,6 +208,25 @@ header{
   top: -40px;
 }
 
+.info{
+  margin-bottom: 20px;
+}
+
+.info__title{
+  font-weight: 600;
+  line-height: 30px;
+}
+
+.info__value{
+  font-size: $font-size + 10px;
+  font-weight: bold;
+}
+
+.error{
+  color: rgb(182, 14, 14);
+}
+
+/** Slider */
 .slider{
   grid-area: slider;
   display: flex;
@@ -185,7 +279,30 @@ footer {
   font-size: $font-size - 1px;
 
   span {
-    color: #102e4a;
+    color: #000;
+  }
+}
+
+.loading{
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  font-size: 40px;
+  color: $primary-color;
+  animation: spinner 0.7s steps(4) infinite;
+}
+
+@keyframes spinner {
+  0% {
+    transform: rotate(0deg);
+  }
+
+  50% {
+    transform: rotate(180deg);
+  }
+
+  100% {
+    transform: rotate(360deg);
   }
 }
 
--- src/main.js
+++ src/main.js
@@ -3,10 +3,10 @@ import App from "./App.vue";
 import router from "./router";
 import store from "./store";
 import { library } from '@fortawesome/fontawesome-svg-core'
-import { faChevronLeft, faChevronRight } from '@fortawesome/free-solid-svg-icons'
+import { faChevronLeft, faChevronRight, faSpinner } from '@fortawesome/free-solid-svg-icons'
 import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
 
-library.add(faChevronRight, faChevronLeft);
+library.add(faChevronRight, faChevronLeft, faSpinner);
 
 createApp(App)
 .use(store)
